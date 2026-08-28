555c6191c170900558f476bf2844a9300b15f561
Marwan Mostafa Khalifa <marwannkhalifa.27@gmail.com>
2026-08-28 18:14:46 +0300

    fixed validation

--- src/validation/auth.validation.js
+++ src/validation/auth.validation.js
@@ -4,7 +4,7 @@ const registerSchema = z.object(
     {
         fullName:z.string().min(2).max(50),
         username:z.string().min(2).max(50),
-        email:z.string().email(),
+        email:z.email(),
         password:z.string().min(8, "Password must at least be 8 characters"),
         sex:z.enum(["male", "female"]).optional(),
         age:z.number().min(18).max(60),
