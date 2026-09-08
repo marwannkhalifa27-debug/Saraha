6de247f05d84204e92947007961d6cdbd8b4be68
Marwan Mostafa Khalifa <marwannkhalifa.27@gmail.com>
2026-09-08 09:11:23 +0300

    fixes

--- src/modules/message/message.controller.js
+++ src/modules/message/message.controller.js
@@ -1,9 +1,19 @@
 import { Router } from "express";
 import { getMessages, sendMessages } from "./message.service.js";
+import { authenticate } from "../../middleware/auth.middleware.js";
+import { validate } from "../../middleware/validation.middleware.js";
+import { messageSchema } from "./message.validation.js";
 
 const messageRouter = Router()
 
-messageRouter.post("/messages/:username", sendMessages)
-messageRouter.get("/messages", getMessages)
+messageRouter.post("/:username", validate(messageSchema) , async (req,res) => {
+    try {
+        await sendMessages(req.params.username, req.body)
+        return res.status(201).json({ message: "Message sent successfully" })
+    } catch (error) {
+        return res.status(error.status || 500).json({ message: error.message })
+    }
+})
+messageRouter.get("/", authenticate ,getMessages)
 
 export default messageRouter
--- src/modules/message/message.service.js
+++ src/modules/message/message.service.js
@@ -1,18 +1,31 @@
 import { messageModel } from "../../DB/models/message.model.js"
+import { userModel } from "../../DB/models/user.model.js"
 
-export const sendMessages = async (req,res,next) => {
+
+export const findUserByEmail = async (username) => await userModel.findOne({ username })
+
+export const sendMessages = async (username, { title , content }) => {
     try {
-        const username = req.body
-        await messageModel.create(username)
-        return res.status(200).json({message:"Message sent successfully"})
-    } catch (error) {
-        return res.status(500).json(error.message)
+    const receiver = await findUserByEmail(username)
+    if(!receiver){
+        throw new Error("User not found")
+    }
+
+    const message = await messageModel.create({
+        title,
+        content,
+        receiverId:receiver._id
+    })
+    return message
+    } 
+    catch (error) {
+        throw new Error(error.message)
     }
 }
 
 export const getMessages = async (req,res,next) => {
     try {
-        const messages = await messageModel.find()
+        const messages = await messageModel.find({ receiverId: req.user.userId })
         return res.status(200).json(messages)
     } catch (error) {
         return res.status(500).json(error.message)
